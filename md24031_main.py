804f9eefa17576a493636ad6a2383278f8fd58bb
marcisd1 <marcisd456@gmail.com>
2025-12-19 23:12:50 +0200

    initial commit

---
+++ md24031_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from md24031_libadd import *
+from md24031_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
